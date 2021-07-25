f7651af2b57e3c0ca7cec4c4f03b5aee00a4f1c9
zstone <zstonex@gmail.com>
2021-07-24 22:24:40 -0400

    subspace imp

--- theories/coverings.v
+++ theories/coverings.v
@@ -13,102 +13,45 @@ Import Order.TTheory GRing.Theory Num.Def  Num.Theory.
 Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
 
-Definition SubspaceDep {X : topologicalType} (A : set X) := {x : X | x \in A} .
+Section WithinExtras.
 
-Program Definition subspaceDep_pointedType 
-    {X : topologicalType} (A : set X) {z} {i : infer (A z)} :=
-  (PointedType (SubspaceDep A) z).
-Next Obligation.  rewrite in_setE; exact: Infer.  Qed.
-Arguments subspaceDep_pointedType {X} (A) {z} {i}.
+Context {X : topologicalType} (A : set X).
 
-Definition inclusion
-    {X : topologicalType} (A : set X) {z} {i : infer (A z)} (a : subspaceDep_pointedType A) : X :=
-  (@proj1_sig X (fun y => y \in A)) a.
-Arguments inclusion {X} (A) {z} {i}.
-
-Definition subspaceDep_topologicalType 
-    {X : topologicalType} (A : set X) {z} {i : infer (A z)} :=
-  @weak_topologicalType  _ X (inclusion A).
-Arguments subspaceDep_topologicalType {X} (A) {z} {i}.
-
-Notation "{  'SubspaceDep' A  }" := (@subspaceDep_topologicalType _ A _ _).
-
-Notation "{  'SubspaceDep' A 'over' z  }" := (@subspaceDep_topologicalType _ A z _).
-
-Notation "{  'SubspaceDep' A 'with' Az  }" := (@subspaceDep_topologicalType _ A _ Az).
-
-Section SubspaceDep .
-Context  {X : topologicalType} (A : set X) {z} {i : infer (A z)}.
-Let j := (@inclusion X A z i) : {SubspaceDep A} -> X.
-
-Lemma inclusion_injective: @injective X {SubspaceDep A} j.
-Proof.  move=> [x ?] [y ?] /=; exact: exist_congr. Qed.
-
-Hint Resolve inclusion_injective : core.
-
-Lemma inclusion_continuous: continuous j.
-Proof. exact: weak_continuous. Qed.
-
-Lemma inclusion_in ( a : {SubspaceDep A}) : A (j a).
-Proof.  by rewrite -in_setE; case: a => ? ? /=. Qed.
-
-Lemma inclusion_preimage ( B : set X): 
-  (j @^-1` B) = [set y | B (j y)]. 
-Proof. by rewrite eqEsubset; split. Qed.
-
-Lemma inclusion_preimage_image ( B : set X): 
-  j @` (j @^-1` B) = A `&` B.
+Lemma within_interior (x : X) : A^° x -> nbhs x = within A (nbhs x).
 Proof.
-rewrite inclusion_preimage eqEsubset; split.
-- by move=> p [[p' /= + + <-]]; rewrite in_setE; split.
-- by move=> p [Ap Bp]; rewrite -in_setE in Ap; exists (exist _ p Ap).
+move=> Aox; rewrite eqEsubset; split.
+- move => W nbhsW.
+  exact: cvg_within.
+- rewrite ?nbhsE => W /= => [[B [+ BsubW ]]].
+  rewrite open_nbhsE => [[oB nbhsB]].
+  exists (B `&` A^°); split.
+  + rewrite open_nbhsE; split.
+    * by apply: openI => //; exact: open_interior.
+    * apply: filterI => //.
+      by move:(open_interior A); rewrite openE; apply.
+  + move=> t /= [] /BsubW + /interior_subset; apply.
 Qed.
 
-Lemma subspace_open (U : set {SubspaceDep A}):
-  open U <-> (exists (V : set X), open V /\ j @` U = A `&` V).
+Lemma within_subset (B : set X) F: 
+  Filter F -> A `<=` B -> within A F `=>` within B F.
 Proof.
-split.
-- rewrite /open /= /wopen /= => [[ V oV <- ]]; exists V; split => //. 
-  exact: inclusion_preimage_image.
-- case=> V [] oV UAV; exists V => //; rewrite inclusion_preimage.
-  move: UAV; rewrite -inclusion_preimage_image eqEsubset /= => [[L R]].
-  rewrite eqEsubset; split => a.
-  + by move=> Vja; rewrite -(image_inj (f:=j)) //; apply: R; exists a.
-  + by move=> Ua; rewrite -(image_inj (f:=j)) //; apply: L; exists a.
-Qed.
-  
-Lemma subspace_open_nbhs (a : {SubspaceDep A}):
-  j @ open_nbhs a `=>` open_nbhs (j a).
-Proof. 
-  move=> U; rewrite open_nbhsE => [[]] oW nbhsU. 
-  split; first (apply: open_comp => // ? ?; exact: inclusion_continuous).
-  rewrite inclusion_preimage /=; exact: nbhs_singleton.
+  move=> FF AsubB W; rewrite /within; apply: filter_app; rewrite nbhs_simpl.
+  by apply: filterE => ? + ?; apply; exact: AsubB.
 Qed.
 
-Lemma subspace_within ( F: set(set({SubspaceDep A}))) (a : {SubspaceDep A}):
+Lemma withinE F: 
   Filter F ->
-  F --> a <-> (within A (j @ F)) --> (j a).
-Proof. 
-move=> FF; split.
-- move=> Fa; apply: cvg_trans; first exact: cvg_within. 
-  apply: cvg_trans; last exact: inclusion_continuous.
-  by apply: cvg_app.
-- move=> AjF W /=; rewrite (nbhsE a) => [[U []]]; rewrite open_nbhsE.
-  case => /subspace_open [V [] oV jU] nbhsU ; rewrite nbhs_simpl => UW.
-  have nbhsV: (nbhs (j a) V). 
-    rewrite openE in oV; apply oV.
-    apply: ( _ : A `&` V `<=` V); first by move=> ?[].
-    by rewrite -jU; exists a => //; exact: nbhs_singleton.
-  apply: (filterS UW).
-  move: (AjF V nbhsV) => /=; rewrite nbhs_simpl. 
-  rewrite /within; near_simpl; apply: filter_app; near=> y.
-  move=>/(_ (inclusion_in y)) VY; move: jU.
-  rewrite eqEsubset => [[] _ /(_ (j y)) /=].
-  case; first (split => //; exact: inclusion_in).
-  by move=> ? ? /inclusion_injective <-.
+  within A F = [set U | (exists V, F V /\ A `&` U = A `&` V)].
+Proof.
+move=> FF; rewrite eqEsubset; split.
+- move=> U Wu; exists ([set x | A x -> U x]); split => //.
+  rewrite eqEsubset; split => t [L R]; split => //; exact: R.
+- move=> U [V [FV AU]]; rewrite /within /prop_near1 nbhs_simpl; near=> w => Aw.
+  suff : (A `&` U) w by case. 
+  rewrite AU; split => //; exact: (near FV).
 Grab Existential Variables. end_near. Qed.
 
-End SubspaceDep.
+End WithinExtras.
 
 Definition Subspace {X : Type} (A : set X) := X.
 
@@ -118,10 +61,26 @@ Context {X : topologicalType} (A : set X).
 Definition subspace_nbhs (x : Subspace A) : set (set (Subspace A)) :=
   if (x \in A) then within A (nbhs x) else globally [set x].
 
+Lemma subspace_nbhs_in (x : X) : 
+  A x -> subspace_nbhs x = within A (nbhs x).
+Proof.
+by rewrite -in_setE /subspace_nbhs; case Ax: (x \in A) => //=.
+Qed. 
+
+Lemma subspace_nbhs_out (x : X) : 
+  ~ A x -> subspace_nbhs x = globally [set x].
+Proof.
+rewrite /subspace_nbhs; case Ax: (x \in A) => //= Q.
+by contradict Q; rewrite -in_setE.
+Qed. 
+
+Ltac split_subspace x := 
+  case (pselect(A x)) => ?; [rewrite subspace_nbhs_in | rewrite subspace_nbhs_out] => //=.
+
 Lemma subspace_nbhs_filter (x : Subspace A) : ProperFilter (subspace_nbhs x).
 Proof.
-rewrite /subspace_nbhs; case Ax: (x \in A). 
-- by apply: within_nbhs_proper; apply: subset_closure; rewrite -in_setE //=.
+split_subspace x.
+- by apply: within_nbhs_proper; apply: subset_closure.
 - exact: globally_properfilter.
 Qed.
 
@@ -135,8 +94,8 @@ Canonical subspace_filteredType :=
 Program Definition subspace_topologicalMixin : Topological.mixin_of (subspace_nbhs)  := 
   @topologyOfFilterMixin (Subspace A) (subspace_nbhs) (subspace_nbhs_filter) _ _.
 Next Obligation.
-move: H; rewrite /subspace_nbhs; case Ap: (p \in A).
-- by move => /nbhs_singleton; apply; rewrite -in_setE.
+move: H; split_subspace p. 
+- by move => /nbhs_singleton; apply.
 - by apply.
 Qed.
 Next Obligation.
@@ -146,16 +105,72 @@ move: H; rewrite /subspace_nbhs; case Ap: (p \in A).
 - by move=> ? x ->; rewrite Ap.
 Qed.
 
-Definition subspace_topologicalType {X : topologicalType} (A : set X) :=
-  TopologicalType _ (subspace_topologicalMixin).
+Canonical subspace_topologicalType :=
+  TopologicalType (Subspace A) (subspace_topologicalMixin).
 
-Lemma subspace_cvg_inP (F : set (set X)) (x : X): 
+Lemma subspace_cvgP (F : set (set X)) (x : X): 
   Filter F -> A x -> 
-  (within A F --> x) <-> (F --> (x : Subspace A)) .
+  (F --> (x : Subspace A)) <-> (F --> within A (nbhs x)).
 Proof.
-move=> FF Ax; split.
-- move=> AFx W /=; rewrite /nbhs /=. 
-  
+move => ??; rewrite /cvg_to/filter_of /= /nbhs /= subspace_nbhs_in //.
+Qed.
+
+Lemma subspace_continuousP { Y : topologicalType} (f : X -> Y): 
+  continuous (f : Subspace A -> Y) <-> 
+  (forall x, A x ->  f @ (within A (nbhs x)) --> f x) .
+Proof.
+split.
+- move=> ctsf x Ax W /=; rewrite nbhs_simpl.
+  by rewrite -subspace_nbhs_in //; apply ctsf.
+- move=> wA x; case (pselect (A x)) => Ax.
+  + apply: cvg_trans; last exact: (wA _ Ax).
+    by apply: cvg_app; rewrite subspace_nbhs_in //.
+  + rewrite /fmap /= /nbhs /= subspace_nbhs_out //= => P /=.
+    by rewrite nbhs_simpl /= => /nbhs_singleton ? ? ->.
+Qed.
+
+Lemma subspace_interior (x : X) : A^° x -> nbhs x = (nbhs (x : Subspace A)).
+Proof.
+move=> Ax; move:(Ax) => /within_interior ->. 
+by rewrite {2}/nbhs /= subspace_nbhs_in //; exact: interior_subset.
+Qed.
+
+Lemma subspace_nbhsP (U : set X) (x : X) : 
+  A x ->
+  nbhs (x : Subspace A) (U) <->
+  (exists V, nbhs (x : X) (V) /\ A `&` U = A `&` V).
+Proof.
+by move=> Ax; rewrite {1}/nbhs /= subspace_nbhs_in // withinE /=; split.
+Qed.
+
+Lemma subspace_open_out (x : Subspace A) : 
+  ~ A x -> open [set x].
+Proof.
+move=> Ax; have : nbhs x [set x] by rewrite /nbhs /= subspace_nbhs_out.
+rewrite nbhsE => [[U [[]]]] oU Ux Usub; suff : U = [set x] by move=> <-.
+by rewrite eqEsubset; split => // t ->.
+Qed.
+
+Lemma subspace_openT : open ( A : set (Subspace A)).
+Proof.
+  move=> x Ax; rewrite subspace_nbhs_in //; apply withinT, nbhs_filter.
+Qed.
+
+Lemma subspace_closedT : closed ( A : set (Subspace A)).
+move=> t cAt; case At: (t \in A); first by rewrite -in_setE. 
+contradict cAt; rewrite /closure /=; apply/existsNP.
+exists [set t]; rewrite not_implyE; split.
+- rewrite /nbhs/= subspace_nbhs_out // => nAt.
+  by rewrite -in_setE At in nAt.
+- apply/set0P/negP/negPn/eqP; rewrite -subset0 => w [+ tw]; rewrite tw.
+  by rewrite -in_setE At.
+Qed.
+
+Lemma subspace_hausdorff : 
+  hausdorff X -> hausdorff [topologicalType of (Subspace A)].
+Proof.
+  rewrite open_hausdorff.
+  move=> hsdfX.
 
 Definition homeomorphism {X Y : topologicalType} (A: set X) (B : set Y) f :=
   {in A &, injective f} /\
